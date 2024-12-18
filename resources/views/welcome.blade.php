d8fb05f1ae2177f9a3edb842072b1c55dd16ba53
bangkit16 <bangkitcaniago@gmail.com>
2024-12-18 08:52:17 +0700

    fik

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-5 col-md-6">
                         <h1 class="text-dark">{{ ('Welcome!') }}</h1>
-                        <p class="text-lead text-light">
+                        <p class="text-lead text-dark">
                             {{ ('Selamat Datang di halaman P3K Safeguard') }}
                         </p>
                     </div>
